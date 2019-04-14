36296a5bc2339d73622c91a672171ccfc49f13ab
Daniel Gohlke <daniel.gohlke@extco.de>
2019-04-14 13:26:13 +0200

    [BUGFIX] Fix exception for l10n_parent when copying an event
    Resolves: #45

--- Configuration/TCA/tx_cartevents_domain_model_event.php
+++ Configuration/TCA/tx_cartevents_domain_model_event.php
@@ -104,6 +104,7 @@ return [
                 ],
                 'foreign_table' => 'tx_cartevents_domain_model_event',
                 'foreign_table_where' => 'AND tx_cartevents_domain_model_event.pid=###CURRENT_PID### AND tx_cartevents_domain_model_event.sys_language_uid IN (-1,0)',
+                'default' => 0,
             ],
         ],
         'l10n_diffsource' => [
