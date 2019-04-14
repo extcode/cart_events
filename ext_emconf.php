2a581a0158b7ba93ff3814c96d478867d6152395
Daniel Gohlke <daniel.gohlke@extco.de>
2019-04-14 14:28:11 +0200

    [TASK] Prepare release
    Resolves: #46

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,23 +1,23 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['cart_events'] = [
     'title' => 'Cart - Events',
     'description' => 'Shopping Cart(s) for TYPO3 - Event Extension',
     'category' => 'plugin',
     'shy' => false,
-    'version' => '1.1.4',
+    'version' => '1.2.0',
     'dependencies' => '',
     'conflicts' => '',
     'priority' => '',
     'loadOrder' => '',
     'module' => '',
-    'state' => 'beta',
+    'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
     'modify_tables' => '',
     'clearcacheonload' => true,
     'lockType' => '',
-    'author' => 'Daniel Lorenz',
+    'author' => 'Daniel Gohlke',
     'author_email' => 'ext.cart@extco.de',
     'author_company' => 'extco.de UG (haftungsbeschränkt)',
     'CGLcompliance' => null,
